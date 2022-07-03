d639ef5f1159bd55f5afa8c71d8f8b48ab7d5da1
Manuela Garcia Yebra <manuelagarciayebra@gmail.com>
2022-07-03 21:15:54 +0200

    add collapsable

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,15 @@
-'use strict';
+"use strict";
 
-const designTitle = document.querySelector('.js_title_design');
-const fillTitle = document.querySelector('.js_title_fill');
-const shareTitle = document.querySelector('.js_title_share');
+const designTitle = document.querySelector(".js_title_design");
+// const fillTitle = document.querySelector('.js_title_fill');
+// const shareTitle = document.querySelector('.js_title_share');
 
-const designContent = document.querySelector('.js_content_design');
-const fillContent = document.querySelector('.js_content_fill');
-const shareContent = document.querySelector('.js_content_share');
+const designContent = document.querySelector(".js_content_design");
+// const fillContent = document.querySelector('.js_content_fill');
+// const shareContent = document.querySelector('.js_content_share');
 
-const arrowIcon = document.querySelector('.js_arrow');
+// const arrowIcon = document.querySelector('.js_arrow');
 
+const handleClick = () => designContent.classList.toggle("collapsed");
+
+designTitle.addEventListener("click", handleClick);
